f868f379c5dd3c1487fe6a48e97a1732fe6f7a79
Alexia Sander <sander.alexia@gmail.com>
2023-07-08 14:46:20 +0900

    validations, relations and seeds done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
+  validates :comment, length: { minimum: 6 }
+  validates :movie, :list, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id, message: "Already bookmarked in this list" } # found on internet
+
   belongs_to :movie
   belongs_to :list
 end
